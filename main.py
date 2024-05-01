47e16b97a968f1e100a4877b32629297d1149876
OksanaO73 <conceria.di.taldom@gmail.com>
2024-05-01 10:44:48 +0300

    На проверку

---
+++ main.py
@@ -0,0 +1,45 @@
+x = 38
+
+print('дратути!')
+if x < 0:
+       print("Меньше нуля")
+print('дотвидания')# This is a sample Python script.
+a, b = 10, 5
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('успех')
+if (a > b) and (a > 0 and b < 1000):
+    print('успех')
+if 5 < b and b < 10:
+    print('успех')
+if '34' > '123':
+    print('успех')
+if '123' > '12':
+    print('успех')
+if [1, 2] > [1, 1]:
+    print('успех')
+
+if '6' > 5:
+    print('успех')
+if [5,6] > 5:
+    print('успех')
+if '6' != 5:
+    print('успех')
+
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
